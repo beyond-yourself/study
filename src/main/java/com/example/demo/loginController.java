950f0f28243b9e1e38cec1615290b332bfc6ff0a
JJ <2977810985@qq.com>
2023-11-03 22:11:08 +0800

    add loginController2

--- src/main/java/com/example/demo/loginController.java
+++ src/main/java/com/example/demo/loginController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 public class loginController {
     public static void main(String[] args) {
-        System.out.println("sdsdsad");
+        System.out.println("sdsddfad");
+
     }
 }
